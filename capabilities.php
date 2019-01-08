39e766a515bb29e8c84aa11110607a07e3fbfe12
Dayan <dev2@pamint.co.cu>
2019-01-07 21:28:40 -0500

    Fix issues.

--- capabilities.php
+++ capabilities.php
@@ -8,6 +8,7 @@
     }
 
     if (isset($_GET['value'])){
+
         $search = $_GET['value'];
         $capDB = new Capabilities();
         if(!$capDB) {
@@ -15,16 +16,24 @@
         }
 
         $sql =<<<EOF
-          SELECT * from capabilities WHERE part_number LIKE  '%$search%' OR description LIKE '%$search%' OR cond LIKE '%$search%';
+          SELECT * from capabilities WHERE pn LIKE  '%$search%'
+          OR alternate_pn LIKE '%$search%' 
+          OR boeing_spec LIKE '%$search%'
+          OR description LIKE '%$search%'
+          OR manufacturer LIKE '%$search%'
+          OR ata LIKE '%$search%';
 EOF;
         $result = [];
         $ret = $capDB->query($sql);
         while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
             $result[] = [
                 'id' => $row['id'],
-                'part_number' => $row['part_number'],
+                'part_number' => $row['pn'],
+                'alternate_pn'=> $row['alternate_pn'],
+                'boeing_spec' => $row['boeing_spec'], 
                 'description' => $row['description'],
-                'cond' => $row['cond']
+                'manufacturer' => $row['manufacturer'],
+                'ata' => $row['ata']
             ];
         }
         header('Content-type: application/json; charset=utf-8');
