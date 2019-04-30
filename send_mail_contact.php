b7b6311e583e91ffb263048b4122da79bb059008
Dayan <dev2@pamint.co.cu>
2019-04-30 13:42:08 -0400

    Add files to send mail functions.

---
+++ send_mail_contact.php
@@ -0,0 +1,26 @@
+<?php
+    try{
+        if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['subject']) || !isset($_POST['message'])){
+            http_redirect('http://apas1.com');
+        }
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+        if(!preg_match($email_exp,$email)) {
+            $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+        }
+        $headers = "From: $email \r\n";
+
+       if ( mail('info@apas1.com',$subject,$message, $headers)){
+           http_redirect('http://apas1.com');
+       }else{
+           http_redirect('http://apas1.com');
+       }
+    }catch(\Exception $e){
+
+    }
+?>
